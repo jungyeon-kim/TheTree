bea90b90833d2596134b140d7c73478760ca8ccf
Kim Jung Yeon <zoemfhs123@gmail.com>
2020-02-16 23:45:24 +0900

    로그 추가
    원활한 디버깅을 위하여 로그를 출력할 수 있도록 매크로 추가

--- Source/TheTree/Private/TheTree.cpp
+++ Source/TheTree/Private/TheTree.cpp
@@ -1,4 +1,5 @@
 #include "TheTree.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(TheTree);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheTree, "TheTree" );
--- Source/TheTree/Public/TheTree.h
+++ Source/TheTree/Public/TheTree.h
@@ -1,3 +1,12 @@
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(TheTree, Log, All);
+#define TTLOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+// 일반 로그
+#define TTLOG_S(Verbosity) UE_LOG(TheTree, Verbosity, TEXT("%s"), *TTLOG_CALLINFO)
+// 출력 로그 (문자열은 *이 붙어야합니다.)
+#define TTLOG(Verbosity, Format, ...) UE_LOG(TheTree, Verbosity, TEXT("%s %s"), *TTLOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+// 조건이 참이 아닌경우 붉은 로그를 띄우고 리턴합니다. 반환 값이 있는 함수에서는 두번째인자로 반환값을 부여해야합니다.
+#define TTCHECK(Expr, ...) { if(!(Expr)) { TTLOG(Error, TEXT("ASSERTION : %s"), TEXT("'"#Expr"'")); return __VA_ARGS__; } }
