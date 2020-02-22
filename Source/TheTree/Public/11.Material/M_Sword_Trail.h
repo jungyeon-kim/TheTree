178bc2716524b261dfef6efd894151c3c4f888a9
kimduckbae <qlccksdlf@gmail.com>
2020-02-22 21:54:20 +0900

    MaterialError
    만약 확인해보고싶다면
    new C++ -> Material 검색하여
    밑에쪽 Material 추가.

---
+++ Source/TheTree/Public/11.Material/M_Sword_Trail.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Materials/Material.h"
+#include "M_Sword_Trail.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THETREE_API UM_Sword_Trail : public UMaterial
+{
+	GENERATED_BODY()
+	
+};
